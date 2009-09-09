000f52b7e66a16158f79bb962232463230da110c
Pedro Kroger <pedro.kroger@gmail.com>
2009-09-09 15:07:40 -0700

    add iterate

--- src/package.lisp
+++ src/package.lisp
@@ -238,6 +238,19 @@
    #:defmacalias
    #:fun
    #:new
+   ;; iterate
+   #:iterate #:iter #:display-iterate-clauses
+   #:defsynonym #:dsetq #:declare-variables
+   #:defmacro-clause #:defmacro-driver #:defclause-sequence
+   #:initially #:after-each #:finally #:finally-protected
+   #:else #:if-first-time #:first-iteration-p #:first-time-p
+   #:finish #:leave #:next-iteration #:next #:terminate
+   #:repeat #:for #:as #:generate #:generating #:in
+   #:sum #:summing #:multiply #:multiplying
+   #:maximize #:minimize #:maximizing #:minimizing #:counting
+   #:always #:never #:thereis #:finding #:collect #:collecting
+   #:with #:while #:until #:adjoining #:nconcing #:appending
+   #:nunioning #:unioning #:reducing #:accumulate #:accumulating
    ))
 
 (in-package :cl-extensions)
